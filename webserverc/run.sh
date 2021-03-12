027c3810ea84d687d853900057893ba7c7a28ec9
astridx <info@astrid-guenther.de>
2021-03-12 20:45:08 +0100

    wip

--- webserverc/run.sh
+++ webserverc/run.sh
@@ -4,18 +4,18 @@
 # @license   http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
 
 ## load .env
-ENVPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-. $ENVPATH/.env 
+#ENVPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+#. $ENVPATH/../.env 
 
-# echo $WEBBASEDIR1
 # Create the www directory as the current user. So all subdirs will inherit the permissions.
-# if [ ! -d $WEBBASEDIR1 ]; then
-#  mkdir $WEBBASEDIR1
+#if [ ! -d $WEBBASEDIR ]; then
+# mkdir $WEBBASEDIR
 #fi
+
 # Create the www directory as the current user. So all subdirs will inherit the permissions.
 if [ ! -d $(dirname $0)/www ]; then
   mkdir $(dirname $0)/www
 fi
 
 # Start the dev server
-docker-compose -f $(dirname $0)/docker-compose.yml up
+docker-compose --env-file ../.env -f $(dirname $0)/docker-compose.yml up
